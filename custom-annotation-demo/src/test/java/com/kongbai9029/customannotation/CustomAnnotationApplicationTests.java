25f8eb819524dd61d325195d7145dd3ce49384f1
YuBWang <15960665735@163.com>
2022-01-24 15:56:33 +0800

    feat(新增模块): 新增自定义注解模块

---
+++ custom-annotation-demo/src/test/java/com/kongbai9029/customannotation/CustomAnnotationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kongbai9029.customannotation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomAnnotationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
