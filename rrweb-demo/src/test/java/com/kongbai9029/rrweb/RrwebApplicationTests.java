674ef97cf61d3c7c36f094ca2558bc37ee880c31
YuBWang <15960665735@163.com>
2022-08-28 11:30:38 +0800

    简单的的登录页面

---
+++ rrweb-demo/src/test/java/com/kongbai9029/rrweb/RrwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kongbai9029.rrweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RrwebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
