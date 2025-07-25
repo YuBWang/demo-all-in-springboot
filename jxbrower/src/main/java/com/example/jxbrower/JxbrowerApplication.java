91e03b062062f72a102aa9bc46dc2fc1464ae045
15960665735@163.com <yubinwang_b@163.com>
2025-07-25 10:30:57 +0800

    jxbrowser 初始化

---
+++ jxbrower/src/main/java/com/example/jxbrower/JxbrowerApplication.java
@@ -0,0 +1,13 @@
+package com.example.jxbrower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JxbrowerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JxbrowerApplication.class, args);
+    }
+
+}
---
+++ jxbrower/src/test/java/com/example/jxbrower/JxbrowerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jxbrower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JxbrowerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
