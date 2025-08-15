ae98ad73989700542d2bc2c55b103fdbb0e1e44a
15960665735@163.com <yubinwang_b@163.com>
2025-08-15 18:24:21 +0800

    guava 实现简单的限流

---
+++ common-util/src/main/java/com/example/commonutil/GuavaRateLimit.java
@@ -0,0 +1,33 @@
+package com.example.commonutil;
+
+import com.google.common.util.concurrent.RateLimiter;
+
+public class GuavaRateLimit {
+    RateLimiter rateLimiter = RateLimiter.create(10);
+    public void doSomething() throws InterruptedException {
+        if (rateLimiter.tryAcquire()) {
+            System.out.println("do something");
+            Thread.sleep(500);
+        } else {
+            System.out.println("too many requests");
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        GuavaRateLimit guavaRateLimit = new GuavaRateLimit();
+        Thread.sleep(2000);
+        for (int i = 0; i < 100; i++) {
+            if (i%20 == 0) {
+                Thread.sleep(2000);
+            }
+            new Thread(() -> {
+                try {
+                    guavaRateLimit.doSomething();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+        }
+    }
+}
+
